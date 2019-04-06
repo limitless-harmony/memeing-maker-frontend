f818fe6b5925ae7b368e3d8c7629d28535cf8f54
Clinton Agada <clinton.agada@andela.com>
2019-04-06 11:14:32 +0100

    Design meme wall, infinite scrolling and some refactoring

---
+++ src/pages/Wall.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import MemeContainer from 'components/MemeContainer';
+import styled from 'styled-components';
+
+import { calculateRem } from 'styles';
+import { tagline } from 'helpers';
+import { dark } from 'styles/colors';
+
+const MemeWall = () => {
+  return (
+    <Container>
+      <Tagline>{tagline}</Tagline>
+      <WallTitle>Building purposeful technology</WallTitle>
+      <MemeContainer />
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: ${calculateRem(18)} auto ${calculateRem(40)};
+  width: 100%;
+`;
+
+const WallTitle = styled.div`
+  margin: ${calculateRem(5)} auto ${calculateRem(50)};
+  padding: ${calculateRem(14)} ${calculateRem(25)};
+  border: ${calculateRem(3)} solid ${dark};
+  border-radius: ${calculateRem(7)};
+  box-sizing: border-box;
+  box-shadow: ${calculateRem(2)} ${calculateRem(4)} ${calculateRem(4)} 0 ${dark};
+  text-align: center;
+  font-size: ${calculateRem(23)};
+  line-height: ${calculateRem(27)};
+  width: 80%;
+`;
+
+const Tagline = styled.div`
+  margin: 0 auto;
+`;
+
+export default MemeWall;
--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import CreateRoute from 'pages/CreateMeme';
 import SingleMeme from 'pages/Single';
+import MemeWall from 'pages/Wall';
 
 const AuthRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AuthRoutes = () => {
       <Switch>
         <Route exact path="/create" component={CreateRoute} />
         <Route exact path="/memes/:id" component={SingleMeme} />
+        <Route exact path="/meme-walls/:id" component={MemeWall} />
       </Switch>
     </React.Fragment>
   );
